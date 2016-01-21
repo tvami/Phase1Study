3f68a543d1b4ba805d85de072c212267e6d6acf5
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-01-21 09:17:05 +0100

    Update 3PilotBladeNtuplizer_test.py

--- test/3PilotBladeNtuplizer_test.py
+++ test/3PilotBladeNtuplizer_test.py
@@ -1,9 +1,7 @@
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: MinBias_13TeV_cfi.py -s RAW2DIGI,L1Reco,RECO --mc --conditions auto:run2_mc --pileup=NoPileUp --magField 38T_PostLS1 
-# --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 -n 10 --no_exec
+# Source: PilotBladeStudy copied and modified
 # Author: Tamas Almos Vami
 # Year: 2015
-# Description: RECO data NTuplizer for Pilot Blade containing data
+# Description: Phase1 MC Simulation NTuplizer
 
 import FWCore.ParameterSet.Config as cms
 
@@ -11,7 +9,7 @@ process = cms.Process('Analysis')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi') 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 #process.load('SimGeneral.MixingModule.mixNoPU_cfi')
@@ -19,8 +17,7 @@ process.load('Configuration.Geometry.GeometrySimDB_cff')
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-#for the Pilot Blade reconstruction include the line below
-process.load('DPGAnalysis.PilotBladeStudy.PilotBladeSetup_cfi')
+
 
 
 # ------------------- Input ----------------------------
@@ -29,15 +26,6 @@ process.maxEvents = cms.untracked.PSet(
   input = cms.untracked.int32(-1)
 )
 
-'''
-# List of input files
-from PoolSource_PB_ppData_RECO import *
-InputFiles = FileNames
-process.source = cms.Source("PoolSource",
-  fileNames = InputFiles,
-  secondaryFileNames = cms.untracked.vstring()
-)
-'''
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring('file:/data/vami/projects/pilotBlade/ppProcessing/PilotBlade_HIFlowCorr_RAWtoRECO_test.root'), #15 event input
   #fileNames = cms.untracked.vstring('file:/data/vami/projects/pilotBlade/ppProcessing/PilotBlade_HIFlowCorr_RAWtoRECO_Run263742_1k.root'), #1k input
@@ -50,7 +38,7 @@ process.options = cms.untracked.PSet(
 
 #----------------Production Info------------------------
 process.configurationMetadata = cms.untracked.PSet(
-  annotation = cms.untracked.string('PilotBladeNtuplizer.py nevts:10'),
+  annotation = cms.untracked.string('Phase1Ntuplizer.py nevts:10'),
   name = cms.untracked.string('Applications'),
   version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -58,27 +46,26 @@ process.configurationMetadata = cms.untracked.PSet(
 
 ## ------------------- Output ---------------------------
 # we do not save the output from the standard sequance 
-# only the output of the NTuplizer (PilotBladeStudy)
+# only the output of the NTuplizer (Phase1Study)
 #-------------------------------------------------------
 
 # --------------------- GlobalTag ----------------------
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 #process.GlobalTag = GlobalTag(process.GlobalTag, '75X_dataRun2_Prompt_v2', '')
 #-------------------------------------------------------
 
 #---------------------- Refitter -----------------------
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
 process.Refitter = process.TrackRefitterP5.clone()
-#process.Refitter.src = 'generalTracks'
-process.Refitter.src = 'hiGeneralTracks' # for HI runs
+process.Refitter.src = 'generalTracks'
 process.Refitter.TrajectoryInEvent = True
 #-------------------------------------------------------
 
-#------------------- PilotBladeStudy -------------------
-process.PilotBladeStudy = cms.EDAnalyzer("PilotBladeStudy",
+#------------------- Phase1Study -------------------
+process.Phase1Study = cms.EDAnalyzer("Phase1Study",
   trajectoryInput = cms.string('Refitter'),
-  fileName = cms.string('nTuplePilotBlade_'+nEvents+'.root'),
+  fileName = cms.string('nTuplePhase1_'+nEvents+'.root'),
   usePixelCPE= cms.bool(True),
 )
 #-------------------------------------------------------
@@ -89,13 +76,13 @@ process.Refitter_step = cms.Path(
   process.Refitter
 )
 
-process.PilotBladeStudy_step = cms.Path(process.PilotBladeStudy)
+process.Phase1Study_step = cms.Path(process.Phase1Study)
 #-------------------------------------------------------
 
 #--------------------- Schedule ------------------------
 process.schedule = cms.Schedule(
   process.Refitter_step,
-  process.PilotBladeStudy_step,	
+  process.Phase1Study_step,	
 )
 
 # customisation of the process
